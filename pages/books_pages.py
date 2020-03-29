feacf73900640ec68d61ab2653bc0df64ebf6c47
Jaskiran Kaur <jkaur372@my.centennialcollege.ca>
2020-03-29 01:25:55 -0400

    Adding parsers

--- pages/books_pages.py
+++ pages/books_pages.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 
 from locators.books_locator import All_Books_Locators
+from parsers.Books import BookParser
 
 class AllBooks:
     def __init__(self,pagecontent):
@@ -8,4 +9,4 @@ class AllBooks:
 
     @property
     def __bool__(self):
-         return self.soup.select(All_Books_Locators.BOOKS)
+         return [BookParser(e) for e in self.soup.select(All_Books_Locators.BOOKS)]
