3ab9178d0599467a3a2b6a97f39f861b0cbe4944
Jaskiran Kaur <jkaur372@my.centennialcollege.ca>
2020-03-31 14:09:18 -0400

    Adding page count

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ import requests
 
 from pages.books_pages import AllBooks
 
+
 page_content=requests.get("http://books.toscrape.com").content
 page=AllBooks(page_content)
-
 books=page.books
-for b in books:
-    print(b)
+
+for x in range(1, page.page_count):
+    url = f"http://books.toscrape.com/catalogue/page-{x+1}.html"
+    page_content = requests.get(url).content
+    page = AllBooks(page_content)
+    books.extend(page.books)
+
--- pages/books_pages.py
+++ pages/books_pages.py
@@ -1,3 +1,4 @@
+import re
 from bs4 import BeautifulSoup
 
 from locators.books_locator import All_Books_Locators
@@ -10,3 +11,11 @@ class AllBooks:
     @property
     def books(self):
          return [BookParser(e) for e in self.soup.select(All_Books_Locators.BOOKS)]
+
+    @property
+    def page_count(self):
+        pager=self.soup.select_one(All_Books_Locators.PAGER).string
+        pattern='Page [0-9]+ of ([0-9]+)'
+        matches=re.search(pattern,pager)
+        pages=int(matches.group(1))
+        return pages
